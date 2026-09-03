a5939a113c48d5e5e23a4647819b6137ed665534
Connor <connor@thinkbreakdigital.net>
2026-09-03 11:52:35 -0400

    Adjusted the app display name and description

--- src/constants/universal-identifiers.ts
+++ src/constants/universal-identifiers.ts
@@ -1,5 +1,5 @@
-export const APP_DISPLAY_NAME = 'Thinkbreak crm';
-export const APP_DESCRIPTION = '';
+export const APP_DISPLAY_NAME = 'ThinkBreak CRM';
+export const APP_DESCRIPTION = 'This project is ThinkBreaks personal organizational CRM, as well as a showcase of the Twenty platform for potential clients.';
 export const APPLICATION_UNIVERSAL_IDENTIFIER = '2d5fcd51-16dd-43ca-a1ac-b1ca5aa2d354';
 export const DEFAULT_ROLE_UNIVERSAL_IDENTIFIER = '378113bd-1355-4d20-b7ec-a6a267f4d0fc';
 export const MAIN_PAGE_FRONT_COMPONENT_UNIVERSAL_IDENTIFIER =
